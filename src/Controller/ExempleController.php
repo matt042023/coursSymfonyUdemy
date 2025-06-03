7e5bd847ad53319f0b9085dc7576287af60b4f2b
matt042023 <matt.lheureux@hotmail.fr>
2025-06-03 17:23:25 +0200

    enregistrement git

---
+++ src/Controller/ExempleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ExempleController extends AbstractController
+{
+    #[Route('/exemple', name: 'app_exemple')]
+    public function index(): Response
+    {
+        return $this->render('exemple/index.html.twig', [
+            'controller_name' => 'ExempleController',
+        ]);
+    }
+}
--- src/Entity/Tuto.php
+++ src/Entity/Tuto.php
@@ -2,37 +2,57 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 use App\Repository\TutoRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TutoRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(normalizationContext: ['groups' => 'tuto:item']),
+        new GetCollection(normalizationContext: ['groups' => 'tuto:list'])
+    ],
+    order: ['name' => 'ASC'],
+    paginationEnabled: false,
+)]
 class Tuto
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['tuto:list', 'tuto:item'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['tuto:list', 'tuto:item'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['tuto:list', 'tuto:item'])]
     private ?string $slug = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['tuto:list', 'tuto:item'])]
     private ?string $subtitle = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['tuto:list', 'tuto:item'])]
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['tuto:list', 'tuto:item'])]
     private ?string $image = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['tuto:list', 'tuto:item'])]
     private ?string $video = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['tuto:list', 'tuto:item'])]
     private ?string $link = null;
 
     public function getId(): ?int
